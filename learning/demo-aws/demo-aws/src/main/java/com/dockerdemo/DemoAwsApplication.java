30d4089b82b573f6e057724f236d0c9f2169e2bf
sherbinsr <sherbinsyles31@gmail.com>
2024-04-29 19:29:46 +0530

    Docker-aws

---
+++ learning/demo-aws/demo-aws/src/main/java/com/dockerdemo/DemoAwsApplication.java
@@ -0,0 +1,28 @@
+package com.dockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/")
+public class DemoAwsApplication {
+	@GetMapping
+	public String applicationStatus() {
+		return "Application is up and running !";
+	}
+
+	@GetMapping("/{name}")
+	public String welcome(@PathVariable String name) {
+		return "Hi " + name + " Welcome to Docker AWS ECS Example";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAwsApplication.class, args);
+	}
+
+}
